818f0b2767ccf251c74104cd870afccd067be48b
npcv1999 <56424406+npcv1999@users.noreply.github.com>
2021-07-06 17:57:37 +0700

    rename "database"

--- functions/index.js
+++ functions/index.js
@@ -1,28 +1,29 @@
+const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 
 admin.initializeApp();
 
-const registrationToken =
-  "dhOy2bxKS-OiMi3GpYZEIX:APA91bF05HxrjA72yfmEhi5uwkg3n2pFRrOcvm_tF83_DfJ-d-nLfFGq4lYYVpxN0kzPrjGx_nq2fteVEYKMkJmyUJgD4FyeVqhrhxoEVEI652_bNU1Ep5txXsrUf2D1n9DfilkSMWSK";
-
-const message = {
-  notification: {
-    title: "Basic Notification",
-    body: "This is a basic notification sent from the server!",
-    imageUrl: "https://my-cdn.com/app-logo.png"
-  },
-  token: registrationToken
-};
-
 // Send a message to the device corresponding to the provided
 // registration token.
-admin
-  .messaging()
-  .send(message)
-  .then((response) => {
-    // Response is a message ID string.
-    console.log("Successfully sent message:", response);
-  })
-  .catch((error) => {
-    console.log("Error sending message:", error);
+
+exports.sendPushNotification = functions.database
+  .ref("/job") // Put your path here with the params.
+  .onWrite(() => {
+    const message = {
+      notification: {
+        title: "Cổng tìm việc",
+        body: "Đã có việc mới mời bạn vào xem"
+        // imageUrl: "https://my-cdn.com/app-logo.png"
+      },
+      topic: "job"
+    };
+    return admin
+      .messaging()
+      .send(message)
+      .then((response) => {
+        console.log("Successfully sent message:", response);
+      })
+      .catch((error) => {
+        console.log("Error sending message:", error);
+      });
   });
